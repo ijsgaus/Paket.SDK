4af3726303c027cd981f9ec66305b17ebce4eeaf
Ivan Simongauz <ijsgaus@gmail.com>
2018-08-29 21:25:40 +0300

    Fot local work

--- test.cmd
+++ test.cmd
@@ -1,3 +1,3 @@
 dotnet pack src\Paket.SDK\Paket.SDK.csproj -o ..\..\nugets
-dotnet nuget push nugets\Paket.SDK.0.0.1-beta1.nupkg --source "d:\Paket.Nuget"
+dotnet nuget push nugets\Paket.SDK.0.0.1-beta1.nupkg --source ".\123"
 dotnet build test\TestApp\TestApp.fsproj -v d
